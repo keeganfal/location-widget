abd5bf64a032bc69bb2159b08d94054d8890c975
keeganfal <60140681+keeganfal@users.noreply.github.com>
2022-09-28 02:19:47 +0100

    weather API function

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [lat, setLat] = useState(null);
   const [lng, setLng] = useState(null);
   const [status, setStatus] = useState(null);
+  const [weatherBasic, setWeatherBasic] = useState({"location":{"name":"","region":"","country":"","lat":51.44,"lon":-0.99,"tz_id":"Europe/London","localtime_epoch":1664309849,"localtime":"2022-09-27 21:17"},"current":{"last_updated_epoch":1664309700,"last_updated":"2022-09-27 21:15","temp_c":7.0,"temp_f":44.6,"is_day":0,"condition":{"text":"Clear","icon":"//cdn.weatherapi.com/weather/64x64/night/113.png","code":1000},"wind_mph":3.8,"wind_kph":6.1,"wind_degree":250,"wind_dir":"WSW","pressure_mb":1000.0,"pressure_in":29.53,"precip_mm":0.0,"precip_in":0.0,"humidity":93,"cloud":0,"feelslike_c":5.0,"feelslike_f":41.1,"vis_km":10.0,"vis_miles":6.0,"uv":1.0,"gust_mph":12.3,"gust_kph":19.8}});
 
   const getLocation = () => {
     if (!navigator.geolocation) {
@@ -23,9 +24,24 @@ function App() {
       }, () => {
         setStatus('Unable to retrieve your location');
       });
+
+      console.log(lat, lng, status);
     }
   }
+
+  const getWeatherBasic = async () => {
+    const key = "1885cb48703e4724a41190049222609";
+    const url = "https://api.weatherapi.com/v1/current.json";
+    let urlWithParams = url + `?key=${key}&q=${lat},${lng}&aqi=no`;
+
+    const res = await fetch(urlWithParams);
+    const data = await res.json();
+
+    setWeatherBasic(data);
+    console.log(data.location.name);
+  };
   
+
   return (
     <div className="App">
       <div className="sidebar">
@@ -33,8 +49,7 @@ function App() {
       </div>
      
       <div className="main">
-        
-        <div className="item-1"><WeatherBasicContainer lat={lat} lng={lng} status={status} getLocation={getLocation}> </WeatherBasicContainer></div>
+        <div className="item-1"><WeatherBasicContainer area={weatherBasic.location.name} region={weatherBasic.location.region} country={weatherBasic.location.country} temp={weatherBasic.current.temp_c} getLocation={getLocation} getWeatherBasic={getWeatherBasic}> </WeatherBasicContainer></div>
         <div className="item-2"><Container text={status}></Container> </div>
         <div className="item-3"><Container text={status}></Container> </div>
       </div>
--- src/components/WeatherBasicContainer/WeatherBasicContainer.jsx
+++ src/components/WeatherBasicContainer/WeatherBasicContainer.jsx
@@ -1,13 +1,14 @@
 import './WeatherBasicContainer.scss';
 
-const WeatherBasicContainer = ({lat, lng, status, getLocation}) => {
-
-  
+const WeatherBasicContainer = ({area, region, country, temp, getLocation, getWeatherBasic}) => {
 
   return (
     <div className="weatherBasicContainer">
-        <p>{lat}{lng}{status}</p> 
+        <p className='area'>{area}, {region}</p> 
+        <p className='country'>{country}</p> 
+        <p className='temp'>{temp}°C</p> 
         <button onClick={getLocation}>Get/Update Location</button>
+        <button onClick={getWeatherBasic}>Get Weather</button>
     </div>
   );
 };
